81822b995569cebf3fbe9fdd2a523f22ea11c361
Padraig Gleeson <p.gleeson@gmail.com>
2013-08-29 10:21:17 +0100

    Tidying up NeuroML2 examples. Run with latest jNeuroML

--- NeuroML2/runlems.sh
+++ NeuroML2/runlems.sh
@@ -1,16 +1,4 @@
-# This assumes you have the latest verisons of jLEMS and NeuroML2 checked out locally, see:
+# Get the latest (binary) version of jNeuroML as outlined here: https://github.com/NeuroML/jNeuroML/blob/master/README.md
 
-# https://github.com/robertcannon/jLEMS
-# https://github.com/NeuroML/NeuroML2
-#
-# Remember to build jLEMS with: ant
-#
-# Usage: ./runlems.sh LEMS_SimpleSpikingNet.xml
-
-JLEMS_HOME=$HOME/jLEMS
-NML2_HOME=$HOME/NeuroML2
-
-export CLASSPATH=$JLEMS_HOME/builtjars/lems.jar
-
-java -Xmx400M -classpath $CLASSPATH org.lemsml.jlemsviz.VizMain -cp $NML2_HOME/NeuroML2CoreTypes $1 $2 $3 $4
+jnml LEMS_SimpleSpikingNet.xml
 
